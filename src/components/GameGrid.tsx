c1e808719a05fae0a9aa9f581275ff23683f6660
vn!ba <itsmescorona@gmail.com>
2023-05-28 11:57:41 +0530

    create custom hook for fetching game

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,34 +1,11 @@
-import  {useEffect, useState} from 'react';
-import apiClient from '../services/api-client.ts';
-import {AxiosError} from 'axios';
+import useGames from '../services/hooks/useGames.ts';
 import {Text} from '@chakra-ui/react';
 
-interface Game {
-    id: number
-    name: string
-}
 
-interface FetchGamesResponse {
-    count: number
-    results: Game[]
-}
 
 function GameGrid() {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const res = await apiClient.get<FetchGamesResponse>('/games')
-                const data = await res.data
-                setGames(data.results)
-            } catch (e) {
-                setError((e as AxiosError).message)
-            }
-        }
-        getData()
-    }, []);
+const {error,games} = useGames()
 
     return (
         <>
