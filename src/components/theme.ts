3827263f05f90f57823ffd8106af2d7bc5b478dc
vn!ba <itsmescorona@gmail.com>
2023-05-29 11:40:41 +0530

    react: card

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -1,9 +1,16 @@
-import {extendTheme,ThemeConfig} from '@chakra-ui/react';
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
-const config:ThemeConfig={
-    initialColorMode:'dark'
-}
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+};
 
-const theme = extendTheme({config})
+const theme = extendTheme({
+  ...config,
+  fonts: {
+    body: "Raleway, sans-serif",
+    heading: "Raleway, sans-serif",
+    // Add more font styles as needed
+  },
+});
 
-export default theme
+export default theme;
